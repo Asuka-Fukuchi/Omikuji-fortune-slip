2f98f007e5c3687e9f726287cb325b4f2d2e03fc
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-11-11 10:29:13 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function getRandomImage() {
+    const number = Math.floor(Math.random() * 7);
+    const imagePath = `./images/omikuji_${number.toString()}.png`;
+    return imagePath;
+}
+
+console.log(getRandomImage());
+
+function playOmikuji() {
+    const timer = setInterval(function(){
+        document.querySelector("#js-result").setAttribute("src", getRandomImage())
+    }, 100);
+
+    setTimeout(function(){
+        clearInterval(timer);
+    }, 3000);
+}
+
+document.querySelector("#js-button").addEventListener("click",playOmikuji)
